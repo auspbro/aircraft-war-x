eb9d353641d3d74e7f1e549caa82ecbfb78b75cf
Ryan Xue <auspbro@gmail.com>
2018-08-30 08:34:11 +0800

    Fix pygame window don't response

--- aircraft_war.py
+++ aircraft_war.py
@@ -8,6 +8,8 @@ import sys
 import time
 import random
 from pygame.locals import *
+# import platform
+# platform.architecture()
 
 
 if __name__ == '__main__':
@@ -27,18 +29,22 @@ if __name__ == '__main__':
     pygame.display.update()
 
     # 绘制玩家的飞机
-    player = pygame.image.load('./res/player1.png')
-    screen.blit(player, (150, 300))
+    hero = pygame.image.load('./res/hero1.png')
+    screen.blit(hero, (150, 300))
 
     # 创建时钟对象
     clock = pygame.time.Clock()
     
     # 游戏循环，以为游戏的正式开始。
     i = 0
-    while True:
-        clock.tick(6)
-        print(i)
-        i += 1
-        pass
+    # while True:
+    #     clock.tick(6)
+    #     print(i)
+    #     i += 1
+    #     pass
 
-    pygame.quit()
+    while True:
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
